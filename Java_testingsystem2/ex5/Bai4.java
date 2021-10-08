e7be4347919f51d9c484013fd9e4a2e0758fd6ab
thaison2000 <80248201+thaison2000@users.noreply.github.com>
2021-10-08 09:59:34 +0700

    Add files via upload

---
+++ Java_testingsystem2/ex5/Bai4.java
@@ -0,0 +1,13 @@
+package ex5;
+
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);  
+    String s;
+
+    System.out.println("nhap vao ngay sinh nhat:");
+    s = scanner.nextLine();
+    }
+}
